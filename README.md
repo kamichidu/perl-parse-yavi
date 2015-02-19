89274894585a8dc733dcd1d6522c0801ae9bfb2a
kamichidu <c.kamunagi@gmail.com>
2015-02-19 18:09:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+utf-8# NAME
+
+Parse::Yavi - It's new $module
+
+# SYNOPSIS
+
+    use Parse::Yavi;
+
+# DESCRIPTION
+
+Parse::Yavi is ...
+
+# LICENSE
+
+Copyright (C) kamichidu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kamichidu <c.kamunagi@gmail.com>
---
+++ lib/Parse/Yavi.pm
@@ -0,0 +1,39 @@
+package Parse::Yavi;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Parse::Yavi - It's new $module
+
+=head1 SYNOPSIS
+
+    use Parse::Yavi;
+
+=head1 DESCRIPTION
+
+Parse::Yavi is ...
+
+=head1 LICENSE
+
+Copyright (C) kamichidu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kamichidu E<lt>c.kamunagi@gmail.comE<gt>
+
+=cut
+
